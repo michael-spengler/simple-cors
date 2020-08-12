b114f34094d2b7b3f0ebdf163e3b04c2eb3f28e7
Michael Spengler <mycamper@gmx.de>
2020-08-12 16:29:54 +0200

    adding executable usage example

---
+++ usage-example.ts
@@ -0,0 +1,9 @@
+import { simpleCors } from "https://deno.land/x/simple-cors/simple-cors.ts";
+import { opine} from "https://deno.land/x/opine/mod.ts";
+
+const app = opine();
+app.use(simpleCors)
+
+app.get('/', (req: any, res: any) => {
+  res.send('Hello World - I accept any request from any origin because I am using simpleCors');
+});

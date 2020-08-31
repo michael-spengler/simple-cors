a3842098e8070703ef8fb81e6b17c1c4cee56e44
michael spengler <43786652+michael-spengler@users.noreply.github.com>
2020-08-31 22:59:53 +0200

    Delete simple-cors.ts

--- simple-cors.ts
+++
@@ -1,8 +0,0 @@
-export const simpleCors = function (req: any, res: any, next: any) {
-
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.set('Access-Control-Allow-Headers', '*');
-
-    next()
-}
